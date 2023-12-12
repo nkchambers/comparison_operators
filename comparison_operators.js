a75c2e0efb9f9304bfae16909ad114762f262a59
nkchambers <nkchames@gmail.com>
2023-12-11 17:08:05 -0800

    adding comparison_operators for 12-11-23

---
+++ comparison_operators.js
@@ -0,0 +1,12 @@
+// CODECADEMY JAVASCRIPT PRACTICE - comparison_operators assignment
+
+// Task 1 - Using let, create a variable named hungerLevel and set it equal to 7.
+let hungerLevel = 7;
+
+// Task 2 - Write an if...else statement using a comparison operator. The condition should check if hungerLevel is greater than 7. 
+// If so, the conditional statement should log, 'Time to eat!'. Otherwise, it should log 'We can eat later!'.
+if (hungerLevel > 7) {
+    console.log("Time to eat!");
+} else {
+    console.log("We can eat later!");
+}
